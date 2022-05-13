db82aac1cac5d8189afe912305b580b79ecc33f1
zhan860127 <zhan860127@gmail.com>
2022-05-13 23:40:45 +0800

    comment the package

--- golang/main.go
+++ golang/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/DataDog/go-python3"
+	//"github.com/DataDog/go-python3"
 
 	"shorten/routers"
 )
 
 func main() {
 	r := routers.SetupRouter()
-	defer python3.Py_Finalize()
+	//defer python3.Py_Finalize()
 
 	//defer python3.Py_Finalize()
 	if err := r.Run(); err != nil {
